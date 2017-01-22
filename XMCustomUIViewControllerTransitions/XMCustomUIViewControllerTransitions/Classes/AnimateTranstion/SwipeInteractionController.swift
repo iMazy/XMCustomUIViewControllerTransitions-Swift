d3c6353db4a00b0fabd999a41d1264011effd18a
Mazy <twt_ios@163.com>
2017-01-22 15:24:41 +0800

    完成手势控制类

--- XMCustomUIViewControllerTransitions/XMCustomUIViewControllerTransitions/Classes/AnimateTranstion/SwipeInteractionController.swift
+++ XMCustomUIViewControllerTransitions/XMCustomUIViewControllerTransitions/Classes/AnimateTranstion/SwipeInteractionController.swift
@@ -10,4 +10,45 @@ import UIKit
 
 class SwipeInteractionController: UIPercentDrivenInteractiveTransition {
 
+    var interactionInProgress: Bool?
+    var shouldCompleteTransition: Bool = false
+    var viewController: UIViewController?
+    
+    func wireToViewController(vc: UIViewController) {
+        viewController = vc
+    }
+    
+    private func prepareGestureRecognizerInView(view: UIView) {
+        let edgePanGesture = UIScreenEdgePanGestureRecognizer.init(target: self, action: #selector(handleGesture))
+        view.addGestureRecognizer(edgePanGesture)
+    }
+    
+    @objc func handleGesture(gesture: UIScreenEdgePanGestureRecognizer) {
+        let translation = gesture.translation(in: gesture.view)
+        var progress = translation.x/200
+        progress = CGFloat(fminf(fmaxf(Float(progress), 0), 1.0))
+        
+        switch gesture.state {
+            
+            case .began:
+                interactionInProgress = true
+                viewController?.dismiss(animated: true, completion: nil)
+            case .changed:
+                shouldCompleteTransition = progress > 0.5
+                update(progress)
+            case .cancelled:
+                interactionInProgress = false
+                cancel()
+            case .ended:
+                self.interactionInProgress = false
+                if !self.shouldCompleteTransition {
+                    self.cancel()
+                } else {
+                    self.finish()
+                }
+            default:
+                break
+        }
+    }
+    
 }
