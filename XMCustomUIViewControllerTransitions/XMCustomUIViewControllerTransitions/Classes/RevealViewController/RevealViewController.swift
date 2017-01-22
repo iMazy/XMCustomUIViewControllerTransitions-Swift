20a8e78383dcf4a3de6e85e3167e781a858d9bba
Mazy <twt_ios@163.com>
2017-01-22 13:49:19 +0800

    完成原始的图片展示功能

--- XMCustomUIViewControllerTransitions/XMCustomUIViewControllerTransitions/Classes/RevealViewController/RevealViewController.swift
+++ XMCustomUIViewControllerTransitions/XMCustomUIViewControllerTransitions/Classes/RevealViewController/RevealViewController.swift
@@ -9,27 +9,24 @@
 import UIKit
 
 class RevealViewController: UIViewController {
+    
+    var yachtCard: YachtCard?
+    
+    @IBOutlet weak var pictureView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        nameLabel.text = yachtCard?.name
+        pictureView.image = UIImage(named: yachtCard?.image ?? "")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func gameOverAction() {
+        dismiss(animated: true, completion: nil)
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
